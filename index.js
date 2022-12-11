940adbd6744dafc56e2d32ce1bbacae38dac1753
Georgie Conway <dopestantelope@gmail.com>
2022-12-11 20:02:36 +0000

    connected db

--- index.js
+++ index.js
@@ -4,22 +4,28 @@ const app = express();
 const http = require('http'); 
 const server = http.createServer(app); 
 const socketio = require("socket.io"); 
-const logger = require('morgan') 
+const logger = require('morgan')
+const mongoose = require('mongoose')
+const connectDB = require('./config/db') 
+
+
+require("dotenv").config({ path: "./config/.env" })
+
 
 app.use(cors()); 
 let io = socketio(server,{ cors: { origin: '*', methods: ['GET', 'POST'] } }); 
 
-//const mongoose = require('mongoose') 
-//const connectDB = require('./config/db') 
-// connectDB() 
+//mongoose deprecation preparation
+mongoose.set('strictQuery', false);
 
-require("dotenv").config({ path: "./config/.env" })
+//connect to database
+connectDB();
+
+app.use(logger("dev"))
 
 app.get('/', (req, res) => { 
   res.send('hello world'); 
 });
-
-app.use(logger("dev"))
 
 io.on('connection', client => { 
   console.log('a user connected');
